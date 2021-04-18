9987c6c4e15598df74d3c7a2f740a5629e7e60ca
elisa33 <pipi.tdfuego@gmail.com>
2021-04-18 13:18:18 -0400

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,46 @@ import 'primeicons/primeicons.css';
 import Layout from './components/Layout/Layout';
 import PetsView from './containers/pages/PetsView/PetsView';
 import FavoritesView from './containers/pages/FavoritesView/FavoriteView';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Router, Route } from 'react-router-dom';
 import ProfileView from './containers/pages/ProfileView/ProfileView';
 import Login from './containers/pages/LogInView/Login';
 import RefugesView from './containers/pages/RefugesView/RefugesView';
+import { createGlobalStyle } from "styled-components"
+import './App.css'
+
+const GlobalStyle = createGlobalStyle`
+    *{
+	box-sizing: border-box;
+    }
+    body {
+	background-color: #f8f7ff;
+	font-family: 'Nunito', sans-serif;
+    }
+	a{
+		text-decoration:none;
+	}
+`
 
 let History = require('history').createBrowserHistory();
 
 function App() {
 	return (
-		<Router history={History}>
-			<Layout>
-				<Route exact path="/" component={PetsView} />
-				<Route path="/pets" component={PetsView} />
-				<Route exact path="/pets/:id" component={PetsView} />
-				<Route path="/favorites" component={FavoritesView} />
-				<Route path="/refuges" component={RefugesView} />
-				<Route path="/profile" component={ProfileView} />
-				<Route path="/login" component={Login} />
-			</Layout>
-		</Router>
+		<Fragment>
+			<GlobalStyle/>
+			<Router history={History}>
+				<Layout>
+					<Route exact path="/" component={PetsView} />
+					<Route path="/pets" component={PetsView} />
+					<Route exact path="/pets/:id" component={PetsView} />
+					<Route path="/favorites" component={FavoritesView} />
+					<Route path="/refuges" component={RefugesView} />
+					<Route path="/profile" component={ProfileView} />
+					<Route path="/login" component={Login} />
+				</Layout>
+			</Router>
+		</Fragment>
+		
 	);
 }
 
---
+++ src/Globals.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700&display=swap');
+
+    *{
+	box-sizing: border-box;
+    }
+    body {
+	background-color: #f8f7ff;
+	font-family: 'Nunito', sans-serif;
+    }
+`
+
+
+
